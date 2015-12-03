7ebb8f0fc2f51b9b520a485c5f32252bd9fdaaf3
Ubuntu <jedi@slateproject.slateproject.g3.internal.cloudapp.net>
2015-12-03 23:42:52 +0000

    db, userinfo, main

--- application/views/assets/js/main.js
+++ application/views/assets/js/main.js
@@ -19,20 +19,16 @@ var app = angular.module('webApp', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
 // Home
-<<<<<<< HEAD
 
-//        .when("/", {templateUrl: "application/views/partials/default.php", controller: "PageCtrl"})
-//        .when("/home", {templateUrl: "application/views/partials/default.php", controller: "PageCtrl"})
-//        .when("/class", {templateUrl: "application/views/partials/class.php", controller: "PageCtrl"}); 
-  
-        .when("/", {templateUrl: "partials/default.php", controller: "PageCtrl"})
-        .when("/home", {templateUrl: "partials/default.php", controller: "PageCtrl"})
-        .when("/class", {templateUrl: "partials/class.php", controller: "PageCtrl"});
-=======
+
         .when("/", {templateUrl: "application/views/partials/default.php", controller: "PageCtrl"})
         .when("/home", {templateUrl: "application/views/partials/default.php", controller: "PageCtrl"})
-        .when("/class", {templateUrl: "application/views/partials/class.php", controller: "PageCtrl"});
->>>>>>> origin/master
+        .when("/class", {templateUrl: "application/views/partials/class.php", controller: "PageCtrl"}); 
+  
+//        .when("/", {templateUrl: "partials/default.php", controller: "PageCtrl"})
+  //      .when("/home", {templateUrl: "partials/default.php", controller: "PageCtrl"})
+    //    .when("/class", {templateUrl: "partials/class.php", controller: "PageCtrl"});
+        
        
 }]);
 
@@ -42,11 +38,7 @@ app.controller('PageCtrl', function($scope, $location, $http) {
 
 app.controller('PeopleCtrl', function ($scope, $http) {
   $scope.people = []
-<<<<<<< HEAD
   $http.get('index.php/home/users')
-=======
-  $http.get('application/views/content.json')
->>>>>>> origin/master
     .success(function(data) {
         $scope.people = data;
         console.log(data);
