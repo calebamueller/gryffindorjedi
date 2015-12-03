b4d83ddfc8e03001ab803690052bff6977e05936
Ubuntu <jedi@slateproject.slateproject.g3.internal.cloudapp.net>
2015-12-03 23:55:34 +0000

    class count

--- application/views/partials/default.php
+++ application/views/partials/default.php
@@ -1,14 +1,12 @@
 <div class="col-sm-9" id="classes">
-        <div id="top" ng-controller="PeopleCtrl">
-          <div ng-repeat="person in people">
+        <div id="top" ng-controller="PeopleCtrl"><?php $count = 1;?>
+          <div ng-repeat="person in people track by $index">
             
             <div class="col-sm-4 item" id="item">
               
-              <h2>{{person.fname}}</h2>
-              <p>{{person.lname}}</p>
-              <p>{{person.class1}}</p>
+              <p>{{person.class<?php echo $count;?>}}</p>
               <p>{{person.notifications}}</p>
-              
+              <?php $count += $count;?>
               <button class="btn btn-outline"><a href="#/class">Enter</a></button>
             </div>
           </div>
