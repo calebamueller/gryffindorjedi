7c1b5ada852359efc0ed790b2b871db6355f2c7a
ljtgy5 <ljtgy5@mail.missouri.edu>
2015-12-04 13:20:30 -0600

    AddGradePage

---
+++ application/views/addGrade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+	</head>
+	<body>
+			<input type="text" name="assignName" value=""></input>
+			<input type="text" name="description" value=""></input>
+			<input type="text" name="totalPoints" value=""></input>
+			
+	</body>
+
+</html>
