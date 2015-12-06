b63ee24cd8cef2593e4eaacbcb3ea441980e7865
ljtgy5 <ljtgy5@mail.missouri.edu>
2015-12-06 16:41:31 -0600

    debugging

--- application/models/Grades_model.php
+++ application/models/Grades_model.php
@@ -9,7 +9,8 @@
 		
 		public function returnAssignments($classID)
 		{
-			$this->db->select('ass_name, ass_description, username, point_possible');
+
+			$this->db->select('ass_name, ass_description, point_possible');
 			$this->db->where('classID', $classID);
 			
 			$query = $this->db->get('assignments');
