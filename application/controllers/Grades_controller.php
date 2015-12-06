23681aa477e267833fc8066e6529b0c9d5f594fd
Ubuntu <jedi@slateproject.slateproject.g3.internal.cloudapp.net>
2015-12-06 20:15:35 +0000

    display all classes

--- application/controllers/Grades_controller.php
+++ application/controllers/Grades_controller.php
@@ -1,8 +1,26 @@
 <?php
 	class Grades_controller extends CI_Controller {
 		
-		$this->load->model('Grades_model');
+		public function __construct()
+  		{
+			parent::__construct();
+			$this->load->database();
+			$this->load->model('Grades_model');
+  		}
 		
+		public function returnResources($classID)
+		{
+			$resourceInfo = $this->Grades_model->returnAssignments($classID);
+		}
 		
+		public function newResource($blobResource, $description, $assignmentName, $pointsPossible)
+		{
+		 	$this->Grades_model->newResource($blobResource, $description, $assignmentName, $pointsPossible);
+		}
+		
+		public function removeResource($resourceID)
+		{
+			//look at codeigniter documentation
+		}
 	}
 ?>
