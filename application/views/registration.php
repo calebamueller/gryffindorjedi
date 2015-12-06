c3aae7d0002c134e834e477a6d1e0e0f5c0746f2
B Esmond <bsey7c@mail.missouri.edu>
2015-12-06 17:57:23 -0600

    Updates to Email and Login/Registration
    -Qui-Gon Jinn

--- application/views/registration.php
+++ application/views/registration.php
@@ -1,36 +1,26 @@
-<h1>Registration</h1>
-<?php  $this->load->helper('form');
-	$form_attributes = array('class' => 'register', 'id' => 'register');
-	echo form_open('logincontrol/register', $form_attributes);
-	$user_data = array('name' => 'username',
-						         'id' => 'username',
-						         'placeholder' => 'Desired Username'
-						  );
-	$password_data = array('name' => 'password',
-							           'id' => 'password',
-							           'placeholder' => 'Password'
-							);
-	$confirm_pass = array ('name' => 'confirm_pass',
-							           'id' => 'confirm_pass',
-							           'placeholder' => 'Confirm Password'
-							);
-	$submit_data = array('name' => 'submit',
-						           'id' => 'submit',
-						           'value' => 'Register'
-						);
-	echo form_input($user_data);
-	echo form_password($password_data);
-	echo form_password($confirm_pass);
-	echo form_submit($submit_data);
-	echo form_close();
-
-	echo '<br /><br />';
-
-	$cancel_attributes = array('class' => 'cancel', 'id' => 'cancel');
-	echo form_open('welcome/index', $cancel_attributes);
-	$cancel_data = array('name' => 'submit',
-						'id' => 'submit',
-						'value' => 'Cancel');
-	echo form_submit($cancel_data);
-	echo form_close();
-?>
+<!DOCTYPE html>
+<html>
+<div id="content">
+<div class="reg_form">
+<div class="form_title">Sign Up</div>
+ <?php echo validation_errors('<p class="error">'); ?>
+ <?php echo form_open("LoginController/register"); ?>
+  <p>
+  <label for="user_name">User Name:</label>
+  <input type="text" id="username" name="username" value="<?php echo set_value('username'); ?>" />
+  </p>
+  <p>
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password" value="<?php echo set_value('password'); ?>" />
+  </p>
+  <p>
+  <label for="con_password">Confirm Password:</label>
+  <input type="password" id="confirm_pass" name="confirm_pass" value="<?php echo set_value('confirm_pass'); ?>" />
+  </p>
+  <p>
+  <input type="submit" value="Submit" />
+  </p>
+ <?php echo form_close(); ?>
+</div><!--<div class="reg_form">-->
+</div><!--<div id="content">-->
+</html>
