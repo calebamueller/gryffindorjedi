5e65f11c45a351e824994e29ce2330290fcb50e1
jcloft <jake.loft18@gmail.com>
2015-12-06 17:21:41 -0600

    all instructors on front page

--- application/views/partials/default.php
+++ application/views/partials/default.php
@@ -7,10 +7,15 @@
                <p>{{person.class1.class_name}}</p>
               <p>{{person.class1.instructor}}</p>
               <p>{{person.class2.class_name}}</p>
+              <p>{{person.class2.instructor}}</p>
               <p>{{person.class3.class_name}}</p>
+              <p>{{person.class3.instructor}}</p>
               <p>{{person.class4.class_name}}</p>
+              <p>{{person.class4.instructor}}</p>
               <p>{{person.class5.class_name}}</p>
+              <p>{{person.class5.instructor}}</p>
              	<p>{{person.class6.class_name}}</p> 
+              <p>{{person.class6.instructor}}</p>
               <button class="btn btn-outline"><a href="#/class">Enter</a></button>
             </div>
           </div>
