bd1543a475ec5c67d60b561bc3382de1fec37414
jcloft <jake.loft18@gmail.com>
2015-12-07 09:40:33 -0600

    echo

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -25,11 +25,11 @@ class Home extends CI_Controller {
     public function announcements($id){
       //$this->load->library('form_validation');
       //$this->load->helper('form');
-      //$announcement = $_POST['announcement'];
+      $announcement = $_POST['announcement'];
       //$id = $_POST['id'];
       //$this->load->model('Classes');
       //$this->db->announce($id, $announcement);
-      echo ("success");
+      echo ($announcement);
     }
 }
 ?>
