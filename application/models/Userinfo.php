16cc7d9d4a0e50abf6592f621454fc30224a7b91
B Esmond <bsey7c@mail.missouri.edu>
2015-12-06 18:22:48 -0600

    fixes

--- application/models/Userinfo.php
+++ application/models/Userinfo.php
@@ -1,44 +1,40 @@
 <?php class Userinfo extends CI_Model {
-  
+
   function __construct() {
     parent::__construct();
     $this->load->database();
   }
-  
+
   public function getUser() {
-<<<<<<< HEAD
-    $query = $this->db->get('user');
-    return $query->result();
-=======
 
 	$array=array();
-	
+
 	$this->db->select('a.class_name, a.instructor');
     	$this->db->from('classes as a');
 	$this->db->join('users as b',"a.class_id = b.class1");
 	$class= $this->db->get();
-		
+
 	foreach ($class->result() as $row){
 		$array[] = array(
 			'class1' => $row
 		);
-	}	
+	}
 	$this->db->select('c.class_name, c.instructor');
 	$this->db->from('classes as c');
 	$this->db->join('users as d', 'c.class_id = d.class2');
 	$class2 = $this->db->get();
-	
+
 	foreach ($class2->result() as $row){
 		$array[] = array(
 			'class2' => $row
 		);
 	}
-	
+
 	$this->db->select('e.class_name, e.instructor');
 	$this->db->from('classes as e');
 	$this->db->join('users as f', 'e.class_id = f.class3');
 	$class3 = $this->db->get();
-	
+
 	foreach ($class3->result() as $row){
 		$array[] = array(
 			'class3' => $row
@@ -58,11 +54,7 @@
 
 	return $array;
 
-	
-    
->>>>>>> ab917b62a6e7065480392b21cfc9a1d637257295
   }
-  
+
   //$this->db->close();
 }
-    
