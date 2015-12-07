ed51d7f862b7b8ed02808ce47ce19f89ad0c50d6
jcloft <jake.loft18@gmail.com>
2015-12-07 09:14:24 -0600

    announce

--- application/ng/partials/announcements.php
+++ application/ng/partials/announcements.php
@@ -1,7 +1,6 @@
 <div ng-controller='AnnounceCtrl'>
-  
-  <form action="index.php/Home/announcements/" method="POST">
-  <textarea name="announcement" value="" ></textarea>
-  <input type='submit' value="Announce!">
+  <form>
+  <textarea name="announcement" value="" ng-model="class.announcement"></textarea>
+  <input type='submit' value="Announce!" ng-click="getID(); update()">
   </form>
 </div>
