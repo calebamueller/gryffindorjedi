eaf50a4e6ad7032899430b4628f45427b7c8c61d
jcloft <jake.loft18@gmail.com>
2015-12-07 09:26:20 -0600

    update

--- application/ng/partials/announcements.php
+++ application/ng/partials/announcements.php
@@ -1,7 +1,8 @@
 <div ng-controller='AnnounceCtrl'>
   <form>
+  <input type="text" value='Class ID' ng-model="classes.id">
   <textarea name="announcement" value="" ng-model="classes.announcement"></textarea>
-  <input type='submit' value="Announce!" ng-click="getID(); update(classes)">
+  <input type='submit' value="Announce!" ng-click="update(classes)">
   </form>
   
   <pre>classes = {{classes | json}}</pre>
