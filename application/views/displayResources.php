8a1ea303383e9e199999cdecd9eddc3edf736b6e
ljtgy5 <ljtgy5@mail.missouri.edu>
2015-12-07 10:46:47 -0600

    resourceView

---
+++ application/views/displayResources.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	</head>
+	<body>
+		<div id="header">
+			<h1>Resources</h1>
+		</div>
+	
+		<nav id="nav">
+			Class Page<br>
+			Home Page<br>
+		</nav>
+	</body>
+
+</html>
