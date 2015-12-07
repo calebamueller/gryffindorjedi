a68531591a38ad7e70512ce43aa033d612542433
B Esmond <bsey7c@mail.missouri.edu>
2015-12-07 13:54:47 -0600

    redirect

--- application/controllers/Grades_controller.php
+++ application/controllers/Grades_controller.php
@@ -1,6 +1,6 @@
 <?php
 	class Grades_controller extends CI_Controller {
-		
+
 		public function __construct()
   		{
 			parent::__construct();
@@ -11,7 +11,7 @@
 			//$this->load->helper('url');
 			$this->load->model('Loginmod');
   		}
-  		
+
   		public function index()
   		{
   			//$uid = ($_POST['user_id']);
@@ -20,54 +20,55 @@
   			$this->load->model('Grades_model');
 			$this->load->view('addGrade');
   		}
-		
+
 		public function returnAssignments()
 		{
 			$this->load->database();
-  			$this->load->model('Grades_model');
+  		$this->load->model('Grades_model');
 			$this->load->view('displayResources');
-			
+
 			//$cid = ($_POST['class_id']);
 			$cid = 1;
-			
+
 			$data = $this->Grades_model->returnAssignments($cid);
-			
+
 			$data = json_encode($this)
-			
+
 			return $data;
 		}
-		
+
 		public function newAssignment()
 		{
 			$this->load->database();
-  			$this->load->model('Grades_model');
-  			$this->load->helper('form');
+  		$this->load->model('Grades_model');
+  		$this->load->helper('form');
 			$this->load->view('addGrade');
-			
+
 			$ass_name = ($_POST['name']);
-   			$description = ($_POST['desc']);
-   			$pointsPoss = ($_POST['pp']);
-   			
-   			$uid = "Jake";
-   			$cid = 1;
-   			
-   			if(strlen($ass_name)<3)
-   			{
-   				
-   			}
-   			
-   			if($pointsPoss<=0)
-   			{
-   				$pointsPoss = 0;
-   			}else if($pointsPoss==null)
-   			{
-   				$pointsPoss = 1;
-   			}
-   			
+ 			$description = ($_POST['desc']);
+ 			$pointsPoss = ($_POST['pp']);
+
+ 			$uid = "Jake";
+ 			$cid = 1;
+
+ 			if(strlen($ass_name)<3)
+ 			{
+
+ 			}
+
+ 			if($pointsPoss<=0)
+ 			{
+ 				$pointsPoss = 0;
+ 			}else if($pointsPoss==null)
+ 			{
+ 				$pointsPoss = 1;
+ 			}
+
 		 	$this->Grades_model->newAssignment($ass_name, $description, $pointsPoss, $uid, $cid);
+			redirect('Home/index', 'refresh');
 
 		}
-		
+
 		public function removeResource($resourceID)
 		{
 			//look at codeigniter documentation
