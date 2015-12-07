473a3c475f6b03aa22fc344b460e51d19fb0573b
jcloft <jake.loft18@gmail.com>
2015-12-06 18:29:38 -0600

    ngclick

--- application/views/partials/default.php
+++ application/views/partials/default.php
@@ -16,7 +16,7 @@
               <p>{{person.class6.class_name}}</p> 
               <p>{{person.class6.instructor}}</p>
               
-              <button class='btn-default' id='{{person.class1.class_name}}{{person.class2.class_name}}{{person.class3.class_name}}{{person.class4.class_name}}'><a href='#/class'>Button</a></button>
+              <button class='btn-default' id='{{person.class1.class_name}}{{person.class2.class_name}}{{person.class3.class_name}}{{person.class4.class_name}}' ng-click='getClass({{person.class1.class_name}}{{person.class2.class_name}}{{person.class3.class_name}}{{person.class4.class_name}})'><a href='#/class'>Button</a></button>
               
             </div>
           </div>
