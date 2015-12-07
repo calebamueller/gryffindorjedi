db3afb2a945b003b379f2b0e79a4683c22494941
jcloft <jake.loft18@gmail.com>
2015-12-06 18:04:51 -0600

    'whitespace'

--- application/views/partials/default.php
+++ application/views/partials/default.php
@@ -5,27 +5,16 @@
             <div class="col-sm-4 item" id="item">
               
               <p>{{person.class1.class_name}}</p>
-              <p>{{person.class1.instructor}}</p>
-              
+              <p>{{person.class1.instructor}}</p>            
               <p>{{person.class2.class_name}}</p>
               <p>{{person.class2.instructor}}</p>
-              
               <p>{{person.class3.class_name}}</p>
-             
               <p>{{person.class3.instructor}}</p>
-              
               <p>{{person.class4.instructor}}</p>
-              
               <p>{{person.class5.class_name}}</p>
               <p>{{person.class5.instructor}}</p>
               <p>{{person.class6.class_name}}</p> 
-              <p>{{person.class6.instructor}}</p>
-
-              
-              
-              
-              
-              
+              <p>{{person.class6.instructor}}</p>  
               
             </div>
           </div>
