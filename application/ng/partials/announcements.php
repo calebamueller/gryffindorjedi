f722a19c262eb7b03b18a6be62bac4a2e2316ce0
jcloft <jake.loft18@gmail.com>
2015-12-07 08:22:20 -0600

    announcements

--- application/ng/partials/announcements.php
+++ application/ng/partials/announcements.php
@@ -1,5 +1,5 @@
-<?php echo form_open('Home/announce');?>
+<?php //echo form_open('Home/announce');?>
 
-<textarea name="announcement" value="<?php set_value('announcement');?>" ></textarea>
+<textarea name="announcement" value="<?php //set_value('announcement');?>" ></textarea>
 <input type='submit' value="Announce!">
-<?php echo form_close();?>
+<?php //echo form_close();?>
