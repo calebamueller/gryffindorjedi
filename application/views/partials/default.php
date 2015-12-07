8419f119fd49ec5a5ead48af8797827d7d3f06f3
jcloft <jake.loft18@gmail.com>
2015-12-06 19:03:33 -0600

    Ass

--- application/views/partials/default.php
+++ application/views/partials/default.php
@@ -16,7 +16,7 @@
               <p>{{person.class6.class_name}}</p> 
               <p>{{person.class6.instructor}}</p>
               
-              <button ng-controller="ClassCtrl" class='btn-default' id='{{person.class1.class_name}}{{person.class2.class_name}}{{person.class3.class_name}}{{person.class4.class_name}}' ng-click='showAlert($event); go(#/class)'>Enter Class</button>
+              <button ng-controller="ClassCtrl" class='btn-default' id='{{person.class1.class_name}}{{person.class2.class_name}}{{person.class3.class_name}}{{person.class4.class_name}}' ng-click='showAlert($event); go('#/class')'>Enter Class</button>
               
             </div>
           </div>
