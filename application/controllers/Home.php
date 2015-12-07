4bdfd0972c441b196f7a11f1a0b6b13d19c7d94c
B Esmond <bsey7c@mail.missouri.edu>
2015-12-07 10:33:30 -0600

    Home changes for login
    -Qui-Gon Jinn

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,33 +10,26 @@ class Home extends CI_Controller {
       $loggedin = $this->session->userdata('loggedin');
       if ($loggedin == TRUE) {
         $this->load->view('home.php');
-
       }
-        //redirect('/#/home','refresh');
+      else{
+        redirect('LoginController/index','refresh');
+      }
     }
 
     public function users() {
       $this->load->database();
       $this->load->model('Userinfo');
-      $this->load->library('session');
-  		$this->load->library('encrypt');
-  		$this->load->helper('security');
-  		$this->load->helper('url');
-
-      $loggedin = $this->session->userdata('loggedin');
-      if ($loggedin == TRUE) {
-        $data = $this->Userinfo->getUser();
-        $info = json_encode($data);
-        echo $info;
-      }
+      $data = $this->Userinfo->getUser();
+      $info = json_encode($data);
+      echo $info;
     }
 
     public function getClass($id) {
       $this->load->database();
       $this->load->model('Classes');
-        $data=$this->Classes->getClassInfo($id);
-        $info = json_encode($data);
-        echo $info;
+      $data=$this->Classes->getClassInfo($id);
+      $info = json_encode($data);
+      echo $info;
     }
 
     public function announcements($id){
