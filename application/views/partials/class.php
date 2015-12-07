ade3ca9392855d435f618d660fbeac3111e694ee
jcloft <jake.loft18@gmail.com>
2015-12-06 22:17:54 -0600

    dumbasses

--- application/views/partials/class.php
+++ application/views/partials/class.php
@@ -3,7 +3,7 @@
     <h2>Due</h2>
     <div class="col-sm-12" id="due">
       <div ng-repeat="c in classes">
-        <h3><script>{{c.assignments.ass_name}}</script><br></h3>
+        <h3>{{c.assignments.ass_name}<br></h3>
       </div>
       <span><h3>Homework 1: </h3><p>{{c.assignments.ass_name}}</p><br></span>
       <h3>Homework 2: </h3><p>September 2<br></p>
